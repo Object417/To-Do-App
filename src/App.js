a23589aab1820049bfe21d5065ce044932612f2a
Object417 <object417.public@gmail.com>
2022-07-19 16:51:30 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+const App = () => {
+  return (
+    <Box component="main">
+      <Typography variant="h1">Hello, React!</Typography>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { AppBar, Toolbar } from "@mui/material"
+
+const Header = () => {
+  return (
+    <AppBar position="sticky">
+      <Toolbar></Toolbar>
+    </AppBar>
+  )
+}
+export default Header
